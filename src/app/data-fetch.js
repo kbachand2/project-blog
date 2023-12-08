dd9d83a74424cd69233ec5bdd7460bf79042bce1
Kyle Bachand <kbachand@sbcglobal.net>
2023-12-07 20:50:27 -0600

    Commented out everything in data-fetch.js due to bugs.

--- src/app/data-fetch.js
+++ src/app/data-fetch.js
@@ -1,23 +1,23 @@
-import { sql } from '@vercel/postgres';
+// import { sql } from '@vercel/postgres';
 
-export async function fetchCount() {
-    // Add noStore() here prevent the response from being cached.
-    // This is equivalent to in fetch(..., {cache: 'no-store'}).
+// export async function fetchCount() {
+//     // Add noStore() here prevent the response from being cached.
+//     // This is equivalent to in fetch(..., {cache: 'no-store'}).
   
-    try {
-      // Artificially delay a response for demo purposes.
-      // Don't do this in production :)
+//     try {
+//       // Artificially delay a response for demo purposes.
+//       // Don't do this in production :)
   
-      // console.log('Fetching revenue data...');
-      // await new Promise((resolve) => setTimeout(resolve, 3000));
+//       // console.log('Fetching revenue data...');
+//       // await new Promise((resolve) => setTimeout(resolve, 3000));
   
-      const data = await sql`SELECT * FROM revenue;`;
+//       const data = await sql`SELECT * FROM revenue;`;
   
-      // console.log('Data fetch complete after 3 seconds.');
+//       // console.log('Data fetch complete after 3 seconds.');
   
-      return data.rows;
-    } catch (error) {
-      console.error('Database Error:', error);
-      throw new Error('Failed to fetch counter data.');
-    }
-  }
+//       return data.rows;
+//     } catch (error) {
+//       console.error('Database Error:', error);
+//       throw new Error('Failed to fetch counter data.');
+//     }
+//   }
